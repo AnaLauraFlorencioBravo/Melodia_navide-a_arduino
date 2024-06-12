838decd763c49146d6b9b06d152d3cc97feed899
AnaLauraFlorencioBravo <82452951+AnaLauraFlorencioBravo@users.noreply.github.com>
2024-06-12 13:59:55 -0600

    Add files via upload

---
+++ melodiaNavidad.ino
@@ -0,0 +1,232 @@
+int altavoz = 11;
+int vel=250;
+void setup(){
+      pinMode(altavoz,OUTPUT); 
+}
+void loop(){
+  tone(altavoz,523,vel/2);
+  delay(vel);
+  tone(altavoz,880,vel/2);
+  delay(vel);
+  tone(altavoz,783,vel/2);
+  delay(vel);
+  tone(altavoz,698,vel/2);
+  delay(vel);
+  tone(altavoz,523,vel/2);
+  delay(vel*2+200);
+
+  tone(altavoz,523,vel/2);
+  delay(vel/2);
+  tone(altavoz,523,vel/2);
+  delay(vel/2);
+
+  tone(altavoz,523,vel/2);
+  delay(vel);
+  tone(altavoz,880,vel/2);
+  delay(vel);
+  tone(altavoz,783,vel/2);
+  delay(vel);
+  tone(altavoz,698,vel/2);
+  delay(vel);
+
+  tone(altavoz,587,vel/2);
+  delay(vel*4);
+    tone(altavoz,587,vel/2);
+  delay(vel);
+
+  
+  tone(altavoz,933,vel/2);
+  delay(vel);
+  tone(altavoz,880,vel/2);
+  delay(vel);
+  tone(altavoz,783,vel/2);
+  delay(vel);
+  tone(altavoz,659,vel/2);
+  delay(vel*2+200);
+
+  tone(altavoz,1046,vel/2);
+  delay(vel/2);  
+  tone(altavoz,1046,vel/2);
+  delay(vel/2); 
+  tone(altavoz,1046,vel/2);
+  delay(vel); 
+  tone(altavoz,1046,vel/2);
+  delay(vel); 
+  tone(altavoz,933,vel/2);
+  delay(vel); 
+  tone(altavoz,783,vel/2);
+  delay(vel);
+  tone(altavoz,880,vel/2);
+  delay(vel*4);
+
+
+
+  tone(altavoz,523,vel/2);
+  delay(vel);
+  tone(altavoz,880,vel/2);
+  delay(vel);
+  tone(altavoz,783,vel/2);
+  delay(vel);
+  tone(altavoz,698,vel/2);
+  delay(vel);
+  tone(altavoz,523,vel/2);
+  delay(vel*2+200);
+
+  tone(altavoz,523,vel/2);
+  delay(vel);
+  tone(altavoz,880,vel/2);
+  delay(vel);
+  tone(altavoz,783,vel/2);
+  delay(vel);
+  tone(altavoz,698,vel/2);
+  delay(vel);
+  tone(altavoz,587,vel/2);
+  delay(vel*2);
+
+
+ tone(altavoz,587,vel/2);
+  delay(vel);
+
+ tone(altavoz,587,vel/2);
+  delay(vel);
+
+
+  tone(altavoz,933,vel/2);
+  delay(vel);
+  tone(altavoz,880,vel/2);
+  delay(vel);
+  tone(altavoz,783,vel/2);
+  delay(vel);
+
+
+
+  tone(altavoz,1046,vel/2);
+  delay(vel);
+    tone(altavoz,1046,vel/2);
+  delay(vel);
+    tone(altavoz,1046,vel/2);
+  delay(vel);
+    tone(altavoz,1046,vel/2);
+  delay(vel);
+    tone(altavoz,1174,vel/2);
+  delay(vel);
+    tone(altavoz,1046,vel/2);
+  delay(vel);
+    tone(altavoz,933,vel/2);
+  delay(vel);
+    tone(altavoz,783,vel/2);
+  delay(vel);
+    tone(altavoz,698,vel/2);
+  delay(vel*2+100);
+
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel*2);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel*2);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,1046,vel/2);
+  delay(vel);
+  tone(altavoz,698,vel/2);
+  delay(vel+vel/2);
+        tone(altavoz,783,vel/2);
+  delay(vel/2);
+        tone(altavoz,880,vel/2);
+  delay(vel*4);
+
+  tone(altavoz,933,vel/2);
+  delay(vel);
+  tone(altavoz,933,vel/2);
+  delay(vel);
+    tone(altavoz,933,vel/2);
+  delay(vel+vel/2);
+     tone(altavoz,933,vel/2);
+  delay(vel/2);
+    tone(altavoz,933,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel/2);
+      tone(altavoz,880,vel/2);
+  delay(vel/2);
+      tone(altavoz,783,vel/2);
+  delay(vel);
+      tone(altavoz,783,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel/2);
+      tone(altavoz,783,vel/2);
+  delay(vel*2);
+      tone(altavoz,1046,vel/2);
+  delay(vel*2);
+
+        tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel/2);
+        tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel/2);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+        tone(altavoz,1046,vel/2);
+  delay(vel);
+        tone(altavoz,698,vel/2);
+  delay(vel+vel/2);
+tone(altavoz,783,vel/2);
+  delay(vel);
+  tone(altavoz,880,vel/2);
+  delay(vel*4);
+
+  tone(altavoz,933,vel/2);
+  delay(vel);
+    tone(altavoz,933,vel/2);
+  delay(vel);
+    tone(altavoz,933,vel/2);
+  delay(vel+vel/2);
+    tone(altavoz,933,vel/2);
+  delay(vel/2);
+    tone(altavoz,933,vel/2);
+  delay(vel);
+    tone(altavoz,880,vel/2);
+  delay(vel);
+      tone(altavoz,880,vel/2);
+  delay(vel);
+
+      tone(altavoz,880,vel/2);
+  delay(vel/2);
+      tone(altavoz,880,vel/2);
+  delay(vel/2);
+
+        tone(altavoz,1046,vel/2);
+  delay(vel);
+
+        tone(altavoz,1046,vel/2);
+  delay(vel);
+          tone(altavoz,933,vel/2);
+  delay(vel);
+            tone(altavoz,783,vel/2);
+  delay(vel);
+              tone(altavoz,698,vel/2);
+  delay(vel*4);
+  
+
+  
+ 
+  }
